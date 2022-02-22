d20eae4b013e66ab89eeaf555b932b142aceb055
xue12311 <1712530035@qq.com>
2022-02-22 23:05:02 +0800

    C语言 学习 半小时

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    double d = 0.0;
+    //字符
+    char c = 'A';
+    //短整型
+    short s = 0;
+    //整型
+    int i = 0;
+    long l = 0;
+    long long ll = 0;
+    float f = 0;
+    //库函数
+    printf("hello world\n");
+    printf("打印字符串: %c \n",c);
+
     return 0;
 }
